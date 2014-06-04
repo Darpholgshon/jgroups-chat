dca4f3710777ddf87d49e56d0569d02c0df5c930
darpholgshon <ralph.hodgson@gmail.com>
2014-06-04 11:12:35 +0100

    tcp2  minimal

--- src/main/java/org/hodgson/test/JGroupsChat.java
+++ src/main/java/org/hodgson/test/JGroupsChat.java
@@ -4,7 +4,6 @@ import org.jgroups.JChannel;
 import org.jgroups.Message;
 import org.jgroups.ReceiverAdapter;
 import org.jgroups.View;
-import org.jgroups.stack.Protocol;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -18,8 +17,6 @@ public class JGroupsChat extends ReceiverAdapter
     static
     {
         System.setProperty("java.net.preferIPv4Stack", "true");
-//        System.setProperty("jgroups.bind_addr","10.253.197.203");
-//        System.setProperty("jgroups.tcpping.initial_hosts", "10.253.197.203[7800], 10.253.197.68[7800]");
     }
 
     JChannel channel;
@@ -28,18 +25,9 @@ public class JGroupsChat extends ReceiverAdapter
     private void start()
             throws Exception
     {
-        channel = new JChannel("tcp-ec2.xml");
+        channel = new JChannel("tcp2.xml");
         channel.setReceiver(this);
         channel.connect("JGroupsChat");
-
-        System.out.println("--------- PROPS --------------");
-        System.out.println(channel.getProperties());
-        System.out.println("-----------------------------");
-
-
-        Protocol protocol = channel.getProtocolStack().findProtocol("TCPPING");
-        System.out.println("****************** PROTOCOL ****************************");
-        System.out.println(protocol.getValue("initial_hosts"));
 
         // Loop til we don't want to talk anymore.
         eventLoop();
